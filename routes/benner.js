297bc6ee69fe91d4a50e261aa2958455f2c7084f
ParthVaghasiya-HHG <hhgsoftechteam09@gmail.com>
2022-04-26 12:45:31 +0530

    parth

--- routes/benner.js
+++ routes/benner.js
@@ -84,4 +84,10 @@ router.post('/kInsertBanner', upload.single('image'), BannerController.kinsert_d
 router.post('/kUpdateBanner', upload.single('image'), BannerController.kUpdate_data);
 router.delete('/kDeleteBanner/:id', upload.single('image'), BannerController.kDelete_data);
 //keyur api end
+
+router.get('/bannerfind',BannerController.banner_find);
+router.get('/Latest',BannerController.latest_find);
+router.get('/Upcoming',BannerController.Upcoming_find);
+
+
 module.exports = router;
