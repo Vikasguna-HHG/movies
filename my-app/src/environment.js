2de9510aa0dab65c46acf05ff749ac2b038b4583
hhgsoftech <hhgsoftechteam10@gmail.com>
2022-05-09 12:03:33 +0530

    make it better

--- my-app/src/environment.js
+++ my-app/src/environment.js
@@ -1,5 +1,5 @@
 //Node live url
 export const env = {
-    // apiURL: 'https://client-hhg1.herokuapp.com/'    
-    apiURL: 'http://localhost:5000/'    
+    apiURL: 'https://server-hhg.    .com/'    
+    // apiURL: 'http://localhost:5000/'    
 };
